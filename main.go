00cfe176990697f168d081f5413cd3a9caba6abd
Ty Bennett <tybennett924@gmail.com>
2025-11-10 01:29:53 -0500

    api call working 401 code need to fix

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
-	"io/util"
-	"net/http"
+	"os"
 	"log"
+	"net/http"
+	"io/ioutil"
 )
 
 func main() {
+ weather_api_key := os.Getenv("WEATHER_API_KEY")
+ latitude := 34.0008
+ longitude := 81.0351
+
+ response, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%f&lon=%f&appid=%s", latitude, longitude, weather_api_key))
+
+ if err != nil {
+	 log.Fatal(err)
+ }
+ defer response.Body.Close()
+
+ body, err := ioutil.ReadAll(response.Body)
+ if err != nil {
+	 log.Fatal(err)
+ }
+	
+ fmt.Println(string(body))
 
 }
 
-func CallWeatherApi(string *apikey, ) {
-
-}
