daa9525cdd9a13477d7b523c5c785f9cbd517352
Ty Bennett <tybennett924@gmail.com>
2025-11-14 17:06:52 -0500

    working ai response and filtering, TODO: construct ai response to send to ollama

--- main.go
+++ main.go
@@ -1,35 +1,74 @@
 package main
 
 import (
-	//"encoding/json"
 	"fmt"
-	"github.com/joho/godotenv"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/valyala/fastjson"
 )
 
+func LogErr(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
 func main() {
+
+	// load env variable for API key
 	err := godotenv.Load()
 	if err != nil {
+		// if error occurs when loading log
 		log.Fatal("Env file could not be loaded")
 	}
 
+	// set api key to the env varibale
 	weather_api_key := os.Getenv("WEATHER_API_KEY")
-	latitude := 34.0008
-	longitude := 81.0351
 
-	response, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%f&lon=%f&appid=%s", latitude, longitude, weather_api_key))
+	// Columbia, SC lat and long
+	const latitude = 34.0007
+	const longitude = -81.0348
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Get request to API with correct Request Params sending those into response and err vars
+	response, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%f&lon=%f&appid=%s&exclude=minutely,hourly,alerts&units=imperial&lang=en", latitude, longitude, weather_api_key))
 
+	// if that err is not nil then log error
+	LogErr(err)
+	//set body to read all response.Body
 	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	fmt.Println(string(body))
+	LogErr(err)
+
+	var p fastjson.Parser
+
+	raw_json, err := p.Parse(string(body))
+	LogErr(err)
+
+	daily_feels_like := raw_json.Get("daily", "0", "feels_like", "day").GetFloat64()
+	daily_humidity := raw_json.Get("daily", "0", "humidity").GetFloat64()
+	// TODO:
+	//	weather_summary := raw_json.Get("daily", "0", "summary").GetStringBytes()
+	//  humidity := raw_json.Get("current")
+
+	var m map[string]string
+	m = make(map[string]string)
+
+	m["daily_feels_like"] = strconv.FormatFloat(daily_feels_like, 'f', -1, 64)
+	m["daily_feels_like"] = strconv.FormatFloat(daily_humidity, 'f', -1, 64)
+	m["daily_feels_like"] = strconv.FormatFloat(daily_feels_like, 'f', -1, 64)
+	m["daily_feels_like"] = strconv.FormatFloat(daily_feels_like, 'f', -1, 64)
+
+	var ai_response strings.Builder
+	ai_response.WriteString("Here is the daily weather forecase for Columbia, SC:\n")
+	ai_response.WriteString("Daily feels like temp: ")
+	ai_response.WriteString(m["daily_feels_like"] + "\n")
+	ai_response.WriteString("Daily high temp: ")
+	ai_response.WriteString(m["daily_humidity"] + "\n")
+
 }
