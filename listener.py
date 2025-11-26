32b5912a7cc6e737ff0ba22b7f60f72e9eb904bc
Ty Bennett <tybennett924@gmail.com>
2025-11-25 23:58:36 -0500

    getting speech recognition setup, big WIP

---
+++ listener.py
@@ -0,0 +1,16 @@
+# program to convert language to api request within google calendar
+# creds are stored in a .env file 
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+    # recognize speech using Sphinx
+    try:
+        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
